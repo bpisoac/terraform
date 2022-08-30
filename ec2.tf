699f11b727c9669d518f291dda1af7ca9b442115
sirfacu <64324089+sirfacu@users.noreply.github.com>
2022-08-30 13:25:00 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-098e42ae54c764c35"
+  ami           = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
   tags = {
     Name = "myEC2withTFCloud",
